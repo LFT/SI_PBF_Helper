fe4723e9a4a4b7162bdff7ec19b919692ea4941d
Py <py.cladiere@telecom-bretagne.eu>
2020-04-14 10:44:48 +0200

    Using subcommands properly

--- si_pbf_helper.py
+++ si_pbf_helper.py
@@ -4,10 +4,67 @@ from models.game import Game
 from starting_data import init_game_power
 
 if __name__ == '__main__':
+    # Commands used during game setup
+    def add_player(args):
+        game.add_player(args.player_name, args.spirit, args.color)
+    
+    # Commands used during the flow of the game    
+    def end_growth():
+        for player_name, player in game.players.items():
+            player.end_growth()
+            
+    def end_turn():
+        for player_name, player in game.players.items():
+            player.end_turn()
+    # Commands used specifically for one player        
+        #add_power_choice
+        #learn_power
+        #reclaim_all
+        #reclaim_one
+        #play_power
+        #forget_power
+        #accelerate_power
+        
+    #    player_name = args.arguments[0]
+    #    del args.arguments[0]
+    #    if not player_name in game.players:
+    #        raise ValueError("Incorrect player name")
+    #    player = game.players[player_name]
+        # energy
+    #    if (args.command == "gain"):
+    #        player.increase_gain(*args.arguments)
+    #    elif (args.command == "energy"):
+    #        player.add_energy(*args.arguments)
+        # powers
+        # elements
+    #    elif (args.command == "element"):
+    #        player.add_innate_element(*args.arguments)
     parser = argparse.ArgumentParser()
     parser.add_argument("name", help="The name of the game that's to be modified")
-    parser.add_argument("command", help="The command you want to run")
-    parser.add_argument("arguments", help="arguments for your command", nargs=argparse.REMAINDER)
+    subparsers = parser.add_subparsers(dest="command")
+    parser_init = subparsers.add_parser('init', help="Initialize the game")
+    parser_growth = subparsers.add_parser('growth', help="End growth")
+    parser_growth.set_defaults(func=end_growth)
+    parser_turn = subparsers.add_parser('turn', help="End turn")
+    parser_turn.set_defaults(func=end_turn)
+    parser_add_player = subparsers.add_parser('player', help="Add a player to the game")    
+    parser_add_player.add_argument('player_name')
+    parser_add_player.add_argument('spirit')
+    parser_add_player.add_argument('color')
+    parser_add_player.set_defaults(func=add_player)
+    parser_increase_gain = subparsers.add_parser('gain', help="Change a player energy gain per turn")
+    parser_add_energy = subparsers.add_parser('energy', help="Change a player energy count")
+    parser_add_innate_element = subparsers.add_parser('element', help="Add one innate element to a player")
+    parser_power_choice = subparsers.add_parser('choice', help="Add X powers to chose from to a player")
+    parser_learn_power = subparsers.add_parser('learn', help="Add a power to the player's hand")
+    parser_reclaim_all = subparsers.add_parser('reclaim_all', help="Reclaim all of a player's powers")
+    parser_reclaim_one = subparsers.add_parser('reclaim_one', help="Reclaime one of a player's power")
+    parser_forget_power = subparsers.add_parser('forget', help="Forget one of a player's power")
+    parser_accelerate_power = subparsers.add_parser('accelerate', help="Accelerate one of a player's power")
+    parser_play_power = subparsers.add_parser('play', help="Play one of a player's power")
+
+    #parser.add_argument("command", help="The command you want to run")
+    #parser.add_argument("arguments", help="arguments for your command", nargs=argparse.REMAINDER)
     args = parser.parse_args()
     filename = args.name + ".data"
     # initialising the Game object. Either a new one or loading the existing one.
@@ -17,40 +74,8 @@ if __name__ == '__main__':
     else :
         with open(filename, "rb") as file:
             game = pickle.load(file)
-
-    # command used during setup
-    if (args.command == "add_player"):
-        game.add_player(*args.arguments)
-    # commands used during the flow of the game
-    elif (args.command == "end_growth"):
-        for player_name, player in game.players.items():
-            player.end_growth()
-    elif (args.command == "end_turn"):
-        for player_name, player in game.players.items():
-            player.end_turn()
-    # all the command below are used specifically for one player
-    else:
-        player_name = args.arguments[0]
-        del args.arguments[0]
-        if not player_name in game.players:
-            raise ValueError("Incorrect player name")
-        player = game.players[player_name]
-        # energy
-        if (args.command == "gain"):
-            player.increase_gain(*args.arguments)
-        elif (args.command == "energy"):
-            player.add_energy(*args.arguments)
-        # powers
-        #add_power_choice
-        #learn_power
-        #reclaim_all
-        #reclaim_one
-        #play_power
-        #forget_power
-        #accelerate_power
-        # elements
-        elif (args.command == "element"):
-            player.add_innate_element(*args.arguments)
+    args.func(args)
+    
     # debugging output
     for player_name, player in game.players.items():
         print(player.name, player.energy)
@@ -64,3 +89,4 @@ if __name__ == '__main__':
             
     with open(filename, "wb") as file:    
         pickle.dump(game, file)
+        
